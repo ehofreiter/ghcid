260557c042b501db89d66783808fc3dc84ae82cc
ehofreiter <35861161+ehofreiter@users.noreply.github.com>
2024-07-16 13:45:12 -0500

    Make VS Code plugin multi-workspace
    TODO: Re-enable the `ghcid.txt` filewatch and harmonize with multi-workspace paradigm.
    
    Update the `haskell-ghcid` VS Code extension to support multiple workspaces.  All changes are limited to the VS Code extension.
    
    - Change behavior of spawning embedded `ghcid` terminal so that it is associated to a given workspace folder (selected by the user).
    - Add two commands to stop Ghcid and clear the associated problems/diagnostics generated by the extension, which can get leftover if the process terminates abnormally.
    - Update README and related documentation.
    - Update various Node package versions.

--- plugins/vscode/README.md
+++ plugins/vscode/README.md
@@ -6,12 +6,17 @@ Shows errors and warnings from [`ghcid`](https://github.com/ndmitchell/ghcid) in
 
 Simply run `ghcid -o ghcid.txt`! `-o` instructs `ghcid` to write its output to a file every time it recompiles your code. This extension will automatically find and watch that file for updates.
 
+TODO: Add this back in!
+
 ## Spawning `ghcid` in VS Code
 
-Alternatively, you can tell VS Code to spawn `ghcid` in an embedded terminal:
+Alternatively, you can tell VS Code to spawn a `ghcid` process in an embedded terminal:
 
-* Get your project working so typing `ghcid` in the project root works. If you need to pass special flags to `ghcid`, create a `.ghcid` file in the project root with the extra flags, e.g. `--command=cabal repl` or similar.
-* Run the VS Code command (`Ctrl+Shift+P`) named "Start Ghcid".
+* Get your project working so typing `ghcid` in the project root works. If you need to pass special flags to `ghcid` you have two options:
+  1. Create a `.ghcid` file in the project root with the extra flags, e.g. `--command=cabal repl` or similar.
+  2. Edit the extension setting `ghcid.command`, which sets the command line used to invoke `ghcid`.
+* Run the VS Code command (`Ctrl+Shift+P`) named "Start Ghcid for given workspace folder" and select the desired workspace folder. Each workspace folder can have a separate `ghcid` process running against it.
+* To stop the embedded `ghcid`(s), run the VS Code command "Stop Ghcid terminal and clear problems for given workspace folder" or "Stop all Ghcid terminals and clear problems". You can also run this to clear extraneous problems from previous crashed instances `ghcid`.
 
 ## Requirements
 
@@ -22,9 +27,9 @@ Requires [`ghcid`](https://github.com/ndmitchell/ghcid) to be installed and on y
 Run:
 
     npm install
-    npm install -g vsce
+    npm install @vscode/vsce
     rm haskell-ghcid-*.vsix
-    vsce package
+    npx @vscode/vsce package
     code --install-extension haskell-ghcid-*.vsix
 
 ## Making releases of this extension
--- plugins/vscode/src/extension.ts
+++ plugins/vscode/src/extension.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as crypto from 'crypto'
+import { isUndefined } from 'util';
 
 function pair<a,b>(a : a, b : b) : [a,b] {return [a,b];}
 
@@ -84,7 +85,7 @@ export function parseGhcidOutput(dir : string, s : string) : [vscode.Uri, vscode
             return f(2,3,2,4);
         if (m = xs[0].match(r3))
             return f(2,3,4,5);
-        return [[new vscode.Uri(), new vscode.Diagnostic(new vscode.Range(0,0,0,0), dedent(xs).join('\n'))]];
+        return [[vscode.Uri.file(dir), new vscode.Diagnostic(new vscode.Range(0,0,0,0), dedent(xs).join('\n'))]];
     }
     return [].concat(... split(lines(s)).map(clean).map(parse));
 }
@@ -103,38 +104,142 @@ function groupDiagnostic(xs : [vscode.Uri, vscode.Diagnostic[]][]) : [vscode.Uri
     return Array.from(seen.values()).sort((a,b) => a[0] - b[0]).map(x => pair(x[1],x[2]));
 }
 
-function watchOutput(root : string, file : string) : fs.FSWatcher {
-    let d = vscode.languages.createDiagnosticCollection('ghcid');
-    let go = () => {
-        d.clear()
-        d.set(groupDiagnostic(parseGhcidOutput(root, fs.readFileSync(file, "utf8")).map(x => pair(x[0], [x[1]]))));
-    };
-    let watcher = fs.watch(file, go);
-    go();
-    return watcher;
-}
+// async function autoWatchGhcidTxt(context: vscode.ExtensionContext) {
+//     // TODO support multiple roots
+//     const watcher = vscode.workspace.createFileSystemWatcher('**/ghcid.txt')
+//     context.subscriptions.push(watcher);
+//     const uri2diags = new Map<string, vscode.DiagnosticCollection>()
+//     context.subscriptions.push({ dispose: () => Array.from(uri2diags.values()).forEach(diag => diag.dispose()) });
 
-async function autoWatch(context: vscode.ExtensionContext) {
-    // TODO support multiple roots
-    const watcher = vscode.workspace.createFileSystemWatcher('**/ghcid.txt')
-    context.subscriptions.push(watcher);
-    const uri2diags = new Map<string, vscode.DiagnosticCollection>()
-    context.subscriptions.push({ dispose: () => Array.from(uri2diags.values()).forEach(diag => diag.dispose()) });
+//     const onUpdate = (uri: vscode.Uri) => {
+//         const diags = uri2diags.get(uri.fsPath) || vscode.languages.createDiagnosticCollection()
+//         uri2diags.set(uri.fsPath, diags)
+//         diags.clear()
+//         diags.set(groupDiagnostic(parseGhcidOutput(path.dirname(uri.fsPath), fs.readFileSync(uri.fsPath, "utf8")).map(x => pair(x[0], [x[1]]))));
+//     }
 
-    const onUpdate = (uri: vscode.Uri) => {
-        const diags = uri2diags.get(uri.fsPath) || vscode.languages.createDiagnosticCollection()
-        uri2diags.set(uri.fsPath, diags)
-        diags.clear()
-        diags.set(groupDiagnostic(parseGhcidOutput(path.dirname(uri.fsPath), fs.readFileSync(uri.fsPath, "utf8")).map(x => pair(x[0], [x[1]]))));
+//     (await vscode.workspace.findFiles('**/ghcid.txt')).forEach(onUpdate)
+//     watcher.onDidCreate(onUpdate)
+//     watcher.onDidChange(onUpdate)
+//     watcher.onDidDelete(uri => {
+//         uri2diags.get(uri.fsPath)?.dispose()
+//         uri2diags.delete(uri.fsPath)
+//     })
+// }
+
+class GhcidProcesses implements vscode.Disposable {
+    readonly processes : Map<string, GhcidProcess>;
+
+    constructor () {
+        this.processes = new Map<string, GhcidProcess>();
     }
 
-    (await vscode.workspace.findFiles('**/ghcid.txt')).forEach(onUpdate)
-    watcher.onDidCreate(onUpdate)
-    watcher.onDidChange(onUpdate)
-    watcher.onDidDelete(uri => {
-        uri2diags.get(uri.fsPath)?.dispose()
-        uri2diags.delete(uri.fsPath)
-    })
+    static getKey(wsFolder: vscode.WorkspaceFolder): string {
+        return wsFolder.uri.toString();
+    }
+
+    addNewProcess(context: vscode.ExtensionContext, wsFolder: vscode.WorkspaceFolder) {
+        this.deleteProcess(wsFolder);
+
+        let ghcidFile = new GhcidFile(context.storagePath, wsFolder);
+
+        let ghcidCommand : string = vscode.workspace.getConfiguration('ghcid', ghcidFile.workspaceFolder.uri).get('command');
+        let opts : vscode.TerminalOptions =
+            os.type().startsWith("Windows") ?
+                {shellPath: "cmd.exe", shellArgs: ["/k", ghcidCommand]} :
+                {shellPath: ghcidCommand, shellArgs: []};
+        opts.name = "ghcid";
+        opts.shellArgs = ["--outputfile=" + ghcidFile.path];
+
+        let terminal = vscode.window.createTerminal(opts);
+        terminal.show();
+
+        let diagnosticCollection = vscode.languages.createDiagnosticCollection('ghcid');
+        let go = () => {
+            diagnosticCollection.clear()
+            diagnosticCollection.set(groupDiagnostic(parseGhcidOutput(ghcidFile.workspaceFolder.uri.toString(), fs.readFileSync(ghcidFile.path, "utf8")).map(x => pair(x[0], [x[1]]))));
+        };
+        let watcher = fs.watch(ghcidFile.path, go);
+        go();
+        let process = new GhcidProcess(ghcidFile, watcher, terminal, diagnosticCollection);
+
+        this.processes.set(GhcidProcesses.getKey(wsFolder), process);
+    }
+
+    deleteProcess(wsFolder: vscode.WorkspaceFolder) {
+        let key = GhcidProcesses.getKey(wsFolder);
+        let process = this.processes.get(key)
+        if (isUndefined(process))
+            return;
+        process.dispose();
+        this.processes.delete(key);
+    }
+
+    dispose() {
+        this.processes.forEach((value : GhcidProcess) => value.dispose());
+        this.processes.clear();
+    }
+}
+
+class GhcidProcess implements vscode.Disposable {
+    readonly ghcidFile : GhcidFile;
+    readonly watcher : fs.FSWatcher;
+    readonly terminal : vscode.Terminal;
+    readonly diagnosticCollection : vscode.DiagnosticCollection;
+
+    constructor(ghcidFile: GhcidFile, watcher: fs.FSWatcher, terminal: vscode.Terminal, diagnosticCollection: vscode.DiagnosticCollection) {
+        this.ghcidFile = ghcidFile;
+        this.watcher = watcher;
+        this.terminal = terminal;
+        this.diagnosticCollection = diagnosticCollection
+    }
+
+    dispose() {
+        this.ghcidFile.dispose();
+        this.watcher.close();
+        this.terminal.dispose();
+        this.diagnosticCollection.dispose();
+    }
+}
+
+class GhcidFile implements vscode.Disposable {
+    readonly workspaceFolder: vscode.WorkspaceFolder;
+    readonly path: string;
+
+    constructor (storagePath: string, folder: vscode.WorkspaceFolder) {
+        this.workspaceFolder = folder;
+
+        // Create temp file "<storagePath>/ghcid-<workspaceFolder-hash>.txt".
+        var hash = crypto.createHash('sha256').update(this.workspaceFolder.uri.toString()).digest('hex').substring(0, 20);
+        this.path = path.join(storagePath, "ghcid-" + hash + ".txt");
+
+        // Initialize to empty file.
+        fs.writeFileSync(this.path, "");
+    }
+
+    dispose() {
+        try {
+            // Delete ghcid file on the filesystem.
+            fs.unlinkSync(this.path);
+        } catch (e) {
+        };
+    }
+}
+
+async function getWorkspaceFolder(context: vscode.ExtensionContext) : Promise<vscode.WorkspaceFolder> {
+    if (!vscode.workspace.workspaceFolders.length) {
+        vscode.window.showWarningMessage("You must open a workspace.")
+        return undefined;
+    }
+
+    let wsFolder = await vscode.window.showWorkspaceFolderPick({ placeHolder: "Run ghcid on which folder?" });
+
+    if (isUndefined(wsFolder)) {
+        vscode.window.showWarningMessage("You must choose a workspace folder.")
+        return undefined;
+    }
+
+    return wsFolder;
 }
 
 // this method is called when your extension is activated
@@ -144,59 +249,44 @@ export async function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
 
-    // Pointer to the last running watcher, so we can undo it
-    var oldWatcher : fs.FSWatcher = null;
-    var oldTerminal : vscode.Terminal = null;
+    // Create storage path if it doesn't exist.
+    if (!fs.existsSync(context.storagePath))
+        fs.mkdirSync(context.storagePath);
 
-    let cleanup = () => {
-        if (oldWatcher != null)
-            oldWatcher.close();
-        oldWatcher = null;
-        if (oldTerminal != null)
-            oldTerminal.dispose();
-        oldTerminal = null;
-    }
-    context.subscriptions.push({dispose: cleanup});
+    // References to managed processes so we can cleanup on demand.
+    var ghcidProcesses = new GhcidProcesses();
+    context.subscriptions.push(ghcidProcesses);
 
-    let add = (name : string, act : () => fs.FSWatcher) => {
-        let dispose = vscode.commands.registerCommand(name, () => {
-            try {
-                cleanup();
-                oldWatcher = act();
-            }
-            catch (e) {
-                console.error("Ghcid extension failed in " + name + ": " + e);
-                throw e;
-            }
-        });
-        context.subscriptions.push(dispose);
-    }
-
-    add('extension.startGhcid', () => {
-        if (!vscode.workspace.rootPath) {
-            vscode.window.showWarningMessage("You must open a workspace first.")
-            return null;
+    context.subscriptions.push(vscode.commands.registerCommand("extension.ghcid.start", async () => {
+        try {
+            let wsFolder = await getWorkspaceFolder(context);
+            if (isUndefined(wsFolder))
+                return;
+            ghcidProcesses.addNewProcess(context, wsFolder);
         }
-        // hashing the rootPath ensures we create a finite number of temp files
-        var hash = crypto.createHash('sha256').update(vscode.workspace.rootPath).digest('hex').substring(0, 20);
-        let file = path.join(os.tmpdir(), "ghcid-" + hash + ".txt");
-        context.subscriptions.push({dispose: () => {try {fs.unlinkSync(file);} catch (e) {};}});
-        fs.writeFileSync(file, "");
+        catch (e) {
+            console.error("ghcid extension failed:" + e);
+            throw e;
+        }
+    }));
 
-        let ghcidCommand : string = vscode.workspace.getConfiguration('ghcid').get('command');
+    context.subscriptions.push(vscode.commands.registerCommand("extension.ghcid.stop", async () => {
+        let wsFolder = await getWorkspaceFolder(context);
+        if (isUndefined(wsFolder))
+            return;
+        vscode.window.showInformationMessage("Stopping ghcid process for workspace folder: " + wsFolder.name);
+        ghcidProcesses.deleteProcess(wsFolder);
+    }))
 
-        let opts : vscode.TerminalOptions =
-            os.type().startsWith("Windows") ?
-                {shellPath: "cmd.exe", shellArgs: ["/k", ghcidCommand]} :
-                {shellPath: ghcidCommand, shellArgs: []};
-        opts.name = "ghcid";
-        opts.shellArgs.push("--outputfile=" + file);
-        oldTerminal = vscode.window.createTerminal(opts);
-        oldTerminal.show();
-        return watchOutput(vscode.workspace.rootPath, file);
-    });
+    context.subscriptions.push(vscode.commands.registerCommand("extension.ghcid.stopAll", async () => {
+        vscode.window.showInformationMessage("Stopping all ghcid processes");
+        ghcidProcesses.dispose();
+    }))
 
-    await autoWatch(context)
+    // TODO: Re-enable this!
+    // Setup watching for ghcid.txt files for when an external process writes a
+    // ghcid.txt file into the workspace.
+    // await autoWatchGhcidTxt(context)
 }
 
 // this method is called when your extension is deactivated
